87e71496b88629b02e207482ecb18593ac045623
situam <martin@situ.am>
2025-05-26 18:47:49 +0200

    first commit of working MiBeAT

---
+++ Button.hpp
@@ -0,0 +1,54 @@
+/*
+ * Button.hpp
+ */
+
+#include "Multiplexer.hpp"
+
+class Button {
+public:
+  // Define the function pointer type
+  using MidiCallback = void (*)(uint8_t, uint8_t);
+
+  // Constructor
+  Button(int pin, int cc_num, MidiCallback send_midi_cc_fn)
+      : pin(pin), cc_num(cc_num), send_midi_cc_fn(send_midi_cc_fn), mem(-1), toggle(0) {
+  }
+
+  // Process the fader movement
+  void process() {
+    setMuxChannel(pin, 1);
+    //delay(1);
+
+    int val = digitalRead(MUX_SIG_1);
+
+
+    if (val != mem) {
+
+if (val==1) { //only handle button when pushed
+
+
+if (toggle==127)
+{
+toggle=0;
+}
+else
+{
+toggle=127;
+}
+      
+send_midi_cc_fn(cc_num, toggle);
+
+}      
+
+mem = val;
+    }
+  }
+
+ int toggle;
+
+private:
+  int pin;
+  int cc_num;
+  int mem;
+  MidiCallback send_midi_cc_fn;
+};
---
+++ RunningAverage.hpp
@@ -0,0 +1,66 @@
+
+//#include <cmath>
+
+class RunningAverage {
+
+public:
+
+    RunningAverage(uint8_t nValues) {
+
+        m_nValues = nValues;
+
+        m_buf = new int[m_nValues];
+
+        clearBuffer();
+
+    }
+
+    ~RunningAverage() {
+
+        delete[] m_buf;
+
+    }
+
+    void addValue(int val) {
+
+        m_buf[m_idx] = val;
+
+        m_idx = (m_idx + 1) % m_nValues;
+
+    }
+
+    int getAverage() {
+
+        int sum = 0;
+
+        for (uint8_t i = 0; i < m_nValues; i++) {
+
+            sum += m_buf[i];
+
+        }
+
+        return static_cast<int>(sum / m_nValues);
+
+    }
+
+    void clearBuffer() {
+
+        for (uint8_t i = 0; i < m_nValues; i++) {
+
+            m_buf[i] = 0;
+
+        }
+
+        m_idx = 0;
+
+    }
+
+private:
+
+    uint8_t m_nValues;
+
+    int* m_buf;
+
+    uint8_t m_idx = 0;
+
+};
