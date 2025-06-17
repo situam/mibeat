359ea843200357e76b26263d99a65f365f850b69
situam <martin@situ.am>
2025-06-17 19:13:38 +0200

    fix and clean up button handler

--- Button.hpp
+++ Button.hpp
@@ -27,33 +27,29 @@ public:
     int val = digitalRead(MUX_SIG_1);
     Serial.printf("%d, ", val);
 
-    if (val != mem)
-    {
-      if (val == 1)
-      { // only handle button when pushed
+    bool is_pressed = (val == 0); // 0 = pressed, 1 = released
 
-        hold_count++;
-        if (hold_count > 10)
-        { // after 10 cycles, send a "hold" message
-          hold_count = 0;
+    if (is_pressed) 
+    { 
+      hold_count++;
 
-          if (toggle == 127)
-          {
-            toggle = 0;
-          }
-          else
-          {
-            toggle = 127;
-          }
+      if (hold_count > 10)
+      {
+        hold_count = 0;
 
-          send_midi_cc_fn(cc_num, toggle);
+        if (toggle == 127)
+        {
+          toggle = 0;
         }
+        else
+        {
+          toggle = 127;
+        }
+        send_midi_cc_fn(cc_num, toggle);
       }
-      else {
-        hold_count = 0; // reset hold count when button is released
-      }
-
-      mem = val;
+    }
+    else {
+      hold_count = 0; // reset hold count when button is released
     }
   }
 
